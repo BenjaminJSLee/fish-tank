c1ef9c9c0cf3ac9d1f725de349e41875a2433ca3
Benjamin Lee <benjaminjslee@gmail.com>
2020-11-08 07:15:13 +0000

    added functionality for bite_fish.js (now eats other fish on collision)

--- public/scripts/entities/bite_fish.js
+++ public/scripts/entities/bite_fish.js
@@ -8,8 +8,21 @@ class BiteFish extends Fish {
   }
   updateOneTick() {
     super.updateOneTick();
+    const ents = this.tank.denizens;
+    for (let fish in ents) {
+      if (ents[fish].isTasty && fish !== this.id &&
+          ents[fish].position.x + ents[fish].width > this.position.x &&
+          ents[fish].position.x < this.position.x + this.width &&
+          ents[fish].position.y + ents[fish].height > this.position.y &&
+          ents[fish].position.y < this.position.y + this.height
+          ) {
+            this.tank.removeDenizen(fish,undefined);
+      }
+    }
+    // on collision, eat a fish and then increase size?
   }
   onClick() {
+    // after on collision is implemented, split size in half after splitting
     var f = new BiteFish({
       tank: this.tank,
       position: this.position,
